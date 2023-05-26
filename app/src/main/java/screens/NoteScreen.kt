e708939d9a0e210268d579ccc8c4737d7c652c96
aiocrew <bodyanski1710@gmail.com>
2023-05-26 03:17:09 +0300

    Note screen

---
+++ app/src/main/java/screens/NoteScreen.kt
@@ -0,0 +1,13 @@
+package screens
+
+
+import androidx.compose.runtime.Composable
+import com.topic2.android.notes.viewmodel.MainViewModel
+
+@Composable
+fun NotesScreen(
+    viewModel: MainViewModel
+){
+
+
+}
